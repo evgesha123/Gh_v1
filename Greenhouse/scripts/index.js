783bb776cb2e7fcf2a1268d2bc8989d856f8f558
Leonov <leonov@mail.ru>
2019-01-21 12:33:53 +0300

    v2
    full example

--- Greenhouse/scripts/index.js
+++ Greenhouse/scripts/index.js
@@ -1,10 +1,11 @@
 
 // scroll arrow button script
+// slowly going/slide to # on page
 
 $(function() {
   $('a[href*=#]').on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 700, 'linear');
   });
 });
 
@@ -12,16 +13,39 @@ $(function() {
 
 
 // navbar scroll sticky
+//
+//   window.onscroll = function() {myFunction()};
+//
+//   var navbar = document.getElementById("navbar");
+//   var sticky = navbar.offsetTop;
+//
+//   function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//   navbar.classList.add("sticky")
+// } else {
+//   navbar.classList.remove("sticky");
+// }
+// }
 
-  window.onscroll = function() {myFunction()};
 
-  var navbar = document.getElementById("navbar");
-  var sticky = navbar.offsetTop;
 
-  function myFunction() {
-  if (window.pageYOffset >= sticky) {
-  navbar.classList.add("sticky")
-} else {
-  navbar.classList.remove("sticky");
-}
-}
+
+
+
+// // slowly going/slide to # on page
+//
+// $(document).ready(function(){
+//   $("#menu").on("click","a", function (event) {
+//     //отменяем стандартную обработку нажатия по ссылке
+//     event.preventDefault();
+//
+//     //забираем идентификатор бока с атрибута href
+//     var id  = $(this).attr('href'),
+//
+//       //узнаем высоту от начала страницы до блока на который ссылается якорь
+//       top = $(id).offset().top;
+//
+//     //анимируем переход на расстояние - top за 1500 мс
+//     $('body,html').animate({scrollTop: top}, 10000);
+//   });
+// });
