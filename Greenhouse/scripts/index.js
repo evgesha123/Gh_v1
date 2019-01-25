33accc32690b1c1051c406de02a9b6eb1cc0dfb8
Leonov <leonov@mail.ru>
2019-01-25 17:36:18 +0300

    v2.3
    interface with clock (online)

--- Greenhouse/scripts/index.js
+++ Greenhouse/scripts/index.js
@@ -19,9 +19,27 @@ $(function() {
 
 
 // -200 это высота до заголовка
-
 // end scroll
 
+
+
+// // time script
+//
+// var hours = document.getElementById("hours");
+// var minutes = document.getElementById("min");
+//
+// function tick() {
+//   var time = new Date();
+//   var h = time.getHours();
+//   var m = time.getMinutes();
+//
+//   hours.innerHTML = h.toString() + " : ";
+//   minutes.innerHTML = m.toString();
+// }
+//
+// setInterval(tick, 300); // что бы была точность, если допустим клиент зайдет на сайт, между сменой секунд
+//
+// // end time script
 
 
 
